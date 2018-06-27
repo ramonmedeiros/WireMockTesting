1b09abb55c195e9c68a7908f46d78df5ec605195
Ramon Medeiros <ramonmedeiros@ciandt.com>
2018-06-27 19:43:29 -0300

    Implement @BeforeClass and @AfterClass
    This will avoid run infrastructure each Scenario on Cucumber
    
    Signed-off-by: Ramon Medeiros <ramonmedeiros@ciandt.com>

--- src/test/java/MockTest.java
+++ src/test/java/MockTest.java
@@ -1,13 +1,52 @@
 package test.java;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
 import cucumber.api.junit.Cucumber;
 import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "feature/mock.feature", 
-glue = "", monochrome = true, dryRun = false)
+@CucumberOptions(features = "feature/mock.feature", glue = "", monochrome = true, dryRun = false)
 public class MockTest {
+
+	private static WireMockServer wireMockServer = new WireMockServer(options().port(9999));
+	private static String content, fileJson = "./resources/__files/request_calculator.json";
+
+	@BeforeClass
+	public static void setUp() {
+
+		// read requesting json
+		try {
+			content = new String(Files.readAllBytes(Paths.get(fileJson)));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// start mock at :9999
+		wireMockServer.start();
+		configureFor("localhost", 9999);
+		wireMockServer.stubFor(post(urlPathEqualTo("/calculadora/calcular")).withRequestBody(equalTo(content))
+				.willReturn(aResponse().withStatus(200).withBody("61")));
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		wireMockServer.stop();
+	}
 }
-
-
--- src/test/java/MockTestSteps.java
+++ src/test/java/MockTestSteps.java
@@ -1,55 +1,25 @@
 package test.java;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
 import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.entity.ContentType;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import cucumber.api.DataTable;
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class MockTestSteps {
 
-	private final WireMockServer wireMockServer = new WireMockServer(options().port(9999));
+	private String response;
 
-	private String content, response, fileJson = "./resources/__files/request_calculator.json";
 	private JSONObject jsonRequest;
-
-	
-	public void setup() throws InterruptedException {
-
-		// read requesting json
-		try {
-			content = new String(Files.readAllBytes(Paths.get(fileJson)));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		// start mock at :9999
-		wireMockServer.start();
-		configureFor("localhost", 9999);
-		wireMockServer.stubFor(post(urlPathEqualTo("/api/books")).withRequestBody(equalTo(content))
-				.willReturn(aResponse().withStatus(200).withBody("61")));
-		Thread.sleep(1000);
-	}
 
 	@Given("^a operacao a ser realizada$")
 	public void a_operacao_a_ser_realizada(DataTable input) throws Throwable {
-		setup();
 		// get headers
 		List<String> headers = input.getGherkinRows().get(0).getCells();
 		jsonRequest = new JSONObject();
@@ -76,22 +46,20 @@ public class MockTestSteps {
 	public void retornar_erro_caso_as_entradas_estejam_invalidas() throws Throwable {
 		// do request and stop server
 		try {
-			Request.Post("http://localhost:9999/api/books")
+			Request.Post("http://localhost:9999/calculadora/calcular")
 					.bodyString(jsonRequest.toString(), ContentType.APPLICATION_JSON).execute().returnContent()
 					.toString();
 		} catch (Exception e) {
 			assertEquals(HttpResponseException.class, e.getClass());
 		}
-		wireMockServer.stop();
 	}
 
 	@Then("^retornar o resultado$")
 	public void retornar_o_resultado() throws Throwable {
 		// do request and stop server
-		response = Request.Post("http://localhost:9999/api/books")
+		response = Request.Post("http://localhost:9999/calculadora/calcular")
 				.bodyString(jsonRequest.toString(), ContentType.APPLICATION_JSON).execute().returnContent().toString();
 		assertEquals(response, "61");
-		wireMockServer.stop();
 	}
 
 }
